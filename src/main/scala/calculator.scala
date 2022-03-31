e8b3cbde44a9f995d9a6e1e36574ca69d485888a
Daniel <daniel.fiedosiuk@gmail.com>
2022-03-31 07:55:15 +0200

    New command objects

--- src/main/scala/calculator.scala
+++ src/main/scala/calculator.scala
@@ -3,9 +3,59 @@ import scala.io.Source
 object calculator extends App {
 
   val lines = Source.fromFile("instructions.txt").getLines.toSeq
-    lines.foldLeft(0){(result, line) =>
-    case line.startsWith
+  val commands = lines.map {
+    case line if line startsWith ("#") =>
+      Comment(line)
+    case line if line.split("\\s+").length == 2 => line match {
+      case line if line startsWith ("+") =>
+        val Array(_, x) = line.split("\\s+")
+        Add(x.toInt)
+      case line if line startsWith ("-") =>
+        val Array(_, x) = line.split("\\s+")
+        Subtract(x.toInt)
+      case line if line startsWith ("*") =>
+        val Array(_, x) = line.split("\\s+")
+        Multiply(x.toInt)
+      case line if line startsWith ("/") =>
+        val Array(_, x) = line.split("\\s+")
+        Divide(x.toInt)
     }
+    case line => Ignore(line)
+  }
 
-  case class Sum (x: Long, y: Long)
+  commands.foreach(println)
+
+//  object Command{
+//    val acceptedCommands = Map{
+//        "+" -> (Add.apply _ ),
+//        "-" -> (Subtract.apply _ ),
+//        "*" -> (Multiply.apply _ ),
+//        "/" -> (Divide.apply _ ),
+//      }
+//    def apply(line :String)={
+//      val symbol = line.split("\\s+").head
+//      acceptedCommands(symbol)(line)
+//    }
+//
+//
+//  }
+
+  case class Comment(s: String)
+
+  case class Add(x: Int)
+//  object Add {
+//    def apply(line: String): Add ={
+//      val Array(_, x) = line.split("\\s+")
+//      Add(x.toInt)
+//    }
+//  }
+
+  case class Subtract(x: Int)
+
+  case class Multiply(x: Int)
+
+  case class Divide(x: Int)
+
+  case class Ignore(s: String)
+
 }
