e3f3043963143cc3105165a712e6325b96c2cc3a
a_szczepanik <a.szczepanik@adform.com>
2022-03-31 10:15:35 +0200

    test for creating objects with values

--- src/test/scala/MainSpec.scala
+++ src/test/scala/MainSpec.scala
@@ -4,10 +4,11 @@ import matchers._
 
 class MainSpec extends AnyFlatSpec with should.Matchers {
 
+  import calculator._
   "calculator.from" should "create values in Objects after spliting lines" in {
-    calculator.Add.from("+ 2") should be(calculator.Add(2))
-    calculator.Subtract.from("- 2") should be(calculator.Subtract(2))
-    calculator.Multiply.from("* 2") should be(calculator.Multiply(2))
-    calculator.Divide.from("/ 2") should be(calculator.Divide(2))
+    Add.from("+ 2") should be(calculator.Add(2))
+    Subtract.from("- 2") should be(calculator.Subtract(2))
+    Multiply.from("* 2") should be(calculator.Multiply(2))
+    Divide.from("/ 2") should be(calculator.Divide(2))
   }
 }
