247a9763ce17beed1c6fade1fca80683180ea2db
a_szczepanik <a.szczepanik@adform.com>
2022-03-31 11:10:19 +0200

    tests for application

---
+++ src/test/scala/CalculatorSpec.scala
@@ -0,0 +1,29 @@
+import org.scalatest._
+
+import flatspec._
+import matchers._
+import scala.io.Source
+
+class MainSpec extends AnyFlatSpec with should.Matchers {
+
+  import calculator._
+
+  "calculator.from" should "create values in Objects after spliting lines" in {
+    Add.from("+ 2") should be(calculator.Add(2))
+    Subtract.from("- 2") should be(calculator.Subtract(2, negation = false))
+    Multiply.from("* 2") should be(calculator.Multiply(2))
+    Divide.from("/ 2") should be(calculator.Divide(2))
+  }
+
+  val lines = Source.fromFile("instructions.txt").getLines.toSeq
+  val result = Seq("+ 2 2", "- 1", "* 4", "+ 2", "/ 3", "/ 2 2")
+  val commands = List(Ignore("+ 2 2"), Add(2), Multiply(5), Subtract(2, negation = true), Divide(1), Ignore("/ 2 2"))
+
+  "lines" should "read lines from file and return as Seq of strings" in {
+    lines should be(result)
+  }
+
+  "calculator.execute" should "execute mathematical operations" in {
+    calculator.execute(commands) should be(-10)
+  }
+}
