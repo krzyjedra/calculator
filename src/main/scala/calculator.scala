f51d5c8a94c2f998c4fc8425fc3c8c00fadfec36
d.fiedosiuk <dfiedosiuk@adform.com>
2022-03-31 10:38:04 +0200

    All commands working

--- src/main/scala/calculator.scala
+++ src/main/scala/calculator.scala
@@ -3,61 +3,49 @@ import scala.io.Source
 object calculator extends App {
 
   val lines = Source.fromFile("instructions.txt").getLines.toSeq
-  //  val commands = lines.map(Command.from)
-  val commands = lines.map {
-    case line if line startsWith ("#") =>
-      Ignore(line)
-    case line if line.split("\\s+").length == 2 => line match {
-      case line if line startsWith ("+") =>
-        val Array(_, x) = line.split("\\s+")
-        Add(x.toInt)
-      case line if line startsWith ("-") =>
-        val Array(_, x) = line.split("\\s+")
-        Subtract(x.toInt)
-      case line if line startsWith ("*") =>
-        val Array(_, x) = line.split("\\s+")
-        Multiply(x.toInt)
-      case line if line startsWith ("/") =>
-        val Array(_, x) = line.split("\\s+")
-        Divide(x.toInt)
-      case line => Ignore(line)
-    }
-    case line => Ignore(line)
-  }
+  val commands = lines.map(Command.from)
 
   commands.foreach(println)
-  println(execute(commands))
+  execute(commands)
 
-  //  object Command {
-  //    val acceptedCommands = Map(
-  //      "+" -> (Add.apply _),
-  //      "-" -> (Subtract.apply _),
-  //      "*" -> (Multiply.apply _),
-  //      "/" -> (Divide.apply _),
-  //    )
-  //
-  //    def from(line: String) = {
-  //      val symbol = line.split("\\s+").head
-  //      val fromMethod = (acceptedCommands(symbol))
-  //      fromMethod(line)
-  //
-  //    }
-  //
-  //  }
+  object Command {
+    val acceptedCommands = Map(
+      "+" -> (Add.from _),
+      "-" -> (Subtract.from _),
+      "*" -> (Multiply.from _),
+      "/" -> (Divide.from _),
+      "#" -> (Ignore.from _),
+      "PRINT" -> (Print.from _)
+    )
 
-  def execute(cmd: Seq[Product]):Double = {
+    def from(line: String) = {
+      val symbol = line.split("\\s+").head
+      val fromMethod = (acceptedCommands(symbol))
+      fromMethod(line)
+    }
+  }
+
+  def execute(cmd: Seq[Product]): Double = {
     cmd.foldLeft(0.0) { (result, n) =>
       n match {
         case a: Add =>
           result + a.x
         case s: Subtract =>
-          result - s.x
+          s.negation match {
+            case true => - result
+            case false => result - s.x
+          }
         case m: Multiply =>
           result * m.x
         case d: Divide =>
           result / d.x
         case i: Ignore =>
           result
+        case p: Print =>
+          println(result)
+          result
+        case n: Negation =>
+          -result
       }
     }
   }
@@ -72,14 +60,17 @@ object calculator extends App {
     }
   }
 
-  case class Subtract(x: Int)
+  case class Subtract(x: Int, negation: Boolean)
 
   object Subtract {
     def from(line: String): Subtract = {
-      val Array(_, x) = line.split("\\s+")
-      Subtract(x.toInt)
+      line.split("\\s+").toList match {
+        case m :: Nil => Subtract(0, true)
+        case m :: n :: Nil => Subtract(n.toInt, false)
+      }
     }
   }
+
 
   case class Multiply(x: Int)
 
@@ -107,4 +98,19 @@ object calculator extends App {
     }
   }
 
+  case class Print(s: String)
+
+  object Print {
+    def from(line: String): Print = {
+      Print(line)
+    }
+  }
+
+  case class Negation()
+
+  object Negation {
+    def from(): Negation = {
+      Negation()
+    }
+  }
 }
